d4e14f7d34d1d95da6ea8e6837c5c4b493c42bd1
Anastasiya Resnickaya <a.kostoyanskaya@yandex.ru>
2024-04-08 17:00:29 +0300

    change

---
+++ post.py
@@ -0,0 +1,22 @@
+def find_amount_platforms(weight, limit):
+    """Возвращает количество платформ, необходимых для перевозки."""
+    weight.sort()
+    count_value = 0
+    count_value = len(weight)
+    left_pointer = 0
+    right_pointer = len(weight) - 1
+    while left_pointer < right_pointer:
+        if weight[left_pointer] + weight[right_pointer] <= limit:
+            left_pointer += 1
+            right_pointer -= 1
+            count_value -= 1
+        else:
+            right_pointer -= 1
+
+    return count_value
+
+
+if __name__ == '__main__':
+    weight = [int(x) for x in input().split()]
+    limit = int(input())
+    print(find_amount_platforms(weight, limit))
