89f8701c332a4543d73b3047afb1534dcf6e241d
Anastasiya Resnickaya <a.kostoyanskaya@yandex.ru>
2024-04-10 20:08:12 +0300

    updated

--- post.py
+++ post.py
@@ -1,13 +1,13 @@
-# Номер успешной посылки 111692299
-def find_amount_platforms(weights, limit):
+# Номер успешной посылки 111778794
+def find_amount_platforms(weights: list[int], limit: int) -> int:
     """Возвращает количество платформ, необходимых для перевозки."""
-    weights_sorted = sorted(weights.copy())
+    weights = sorted(weights)
     count_value = len(weights)
     light_pointer = 0
     heavy_pointer = count_value - 1
     while light_pointer < heavy_pointer:
-        if (weights_sorted[light_pointer] +
-                weights_sorted[heavy_pointer] <= limit):
+        if (weights[light_pointer] +
+                weights[heavy_pointer] <= limit):
             light_pointer += 1
             count_value -= 1
         heavy_pointer -= 1
@@ -17,4 +17,3 @@ def find_amount_platforms(weights, limit):
 if __name__ == '__main__':
     print(find_amount_platforms(weights=[int(x) for x in input().split()],
                                 limit=int(input())))
-
